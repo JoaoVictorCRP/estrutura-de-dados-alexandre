b93823d46630b3a5e0108d825ae7ed1676e51318
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-02-15 23:00:25 -0300

    Aula 0

---
+++ 0-RecapitulandoConceitos/aula0.js
@@ -0,0 +1,15 @@
+        // Tipagem de variáveis
+         
+        const nome = "João Victor";   // String
+        const idade = 19;     // Integer
+        const peso = 88.9;    // Double
+        const masculino = true; // Boolean
+        const pessoas = ['José', 'Rafael', 'Thiago']; // Array
+        const cadastro = {      // Objeto
+            usuario: 'JoaoVictorCRP',
+            senha: 123,
+            administrador: false,
+            // Chave : valor
+        };
+
+        console.log(cadastro.senha); // Acessando para acessar um valor dentro de um objeto, utilize: <objeto>.<chave>
