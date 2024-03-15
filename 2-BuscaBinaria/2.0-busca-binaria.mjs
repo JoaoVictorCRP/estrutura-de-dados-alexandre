52c9d49d5a878080b4a4aedd3a753679aff3cac0
Joao Victor <joaovictorxl120@gmail.com>
2024-03-14 21:22:40 -0300

    comparacao de validacoes

--- 2-BuscaBinaria/2.0-busca-binaria.mjs
+++ 2-BuscaBinaria/2.0-busca-binaria.mjs
@@ -1,28 +1,36 @@
+let comps;
+
 function buscaBinaria(valorBusca, vetor){
     let inicio = 0;
     let fim = vetor.length - 1;
+    comps = 0;
 
     while(fim>=inicio){ 
-
+        comps++
         let meio = Math.floor((inicio+fim)/2) // Pegando o meio do vetor
         
         if(valorBusca === vetor[meio]){
+            comps++
             return meio // Encontrou
         } 
         else if(valorBusca > vetor[meio]) {
+            comps += 2
             inicio = meio+1 // O Meio do vetor é menor que o valor buscado, ande para a direita.
         } else {
+            comps += 2
             fim = meio-1 // O meio do vetor é maior que o valor buscado, ande para a esquerda.
         }
     };
     return -1 // Não foi encontrado...
 }
 
-const nums = [0, 11, 22, 33, 44, 55, 66, 77, 88, 99];
+// const nums = [0, 11, 22, 33, 44, 55, 66, 77, 88, 99];
+// console.log(`Posição de 33: ${buscaBinaria(33, nums)}`)
 
-console.log(`Posição de 33: ${buscaBinaria(33, nums)}`)
-console.log(`Posição de 11: ${buscaBinaria(33, nums)}`)
-console.log(`Posição de 88: ${buscaBinaria(33, nums)}`)
+import {nomes} from '../data/vetor-nomes.mjs'
+console.log(`Posição de ALEXANDRE: ${buscaBinaria('ALEXANDRE', nomes)}, Comparações realizadas: ${comps}`)
+
+
 
 // Melhor caso:
 // Binária --> Elemento do meio
