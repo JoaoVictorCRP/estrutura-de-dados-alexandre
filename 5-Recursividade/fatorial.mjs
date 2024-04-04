5e3191ece10316759d50629e55afa4c31e064f2f
Joao Victor <joaovictorxl120@gmail.com>
2024-04-04 19:57:48 -0300

    fatorial recursivo

--- 5-Recursividade/fatorial.mjs
+++ 5-Recursividade/fatorial.mjs
@@ -2,9 +2,20 @@ function fatorial_normal(n){
     // Eis aqui uma função "tradicional" do fatorial, qual estamos acostumados. Não é mesmo?
     let res=1;
     for(let i=n;i>1;i--){
-        res = res*i
-    }
-    return res
-}
-
+        res = res*i;
+    };
+    return res;
+};
 console.log(`O fatorial de 5 é ${fatorial_normal(5)}`);
+
+
+// Vamos a recursividade!
+function fat_recursivo(n){
+    if(n==1){
+        return n
+    } else {
+        return n * fat_recursivo(n-1)
+    }
+};
+
+console.log(`O Fatorial (recursivo) de 8 é ${fat_recursivo(8)}`)
