cb3ef4349a838d6c63d326826cee4abdd1b5a021
Joao Victor <joaovictorxl120@gmail.com>
2024-05-09 18:51:56 -0300

    quicksort

--- 7-QuickSort/7.0-quickSort.mjs
+++ 7-QuickSort/7.0-quickSort.mjs
@@ -1,7 +1,7 @@
 function quickSort(vetor, ini = 0, fim = vetor.length-1){
     if(fim<=ini) return; // Caso base
 
-    const pivot = Math.floor(Math.random()) * (0 + 10) - 0; // O pivot pode ser qualquer posição
+    const pivot = fim; // O pivot pode ser qualquer posição
     console.log('o pivot é: ')
     let div = ini - 1;
 
@@ -23,37 +23,7 @@ function quickSort(vetor, ini = 0, fim = vetor.length-1){
     quickSort(vetor, ini, div-1) // Menores que o pivot (esquerda)
     quickSort(vetor, div+1, fim) // Maiores que o pivot (direita)
 };
-const nums = [2,5,7,1,6,3,4]
-// const nums = [3,4,2,1]
 
-quickSort(nums)
-
-console.log(nums)
-function quickSort(vetor, ini = 0, fim = vetor.length-1){
-    if(fim<=ini) return; // Caso base
-
-    const pivot = Math.floor(Math.random()) * (0 + 10) - 0; // O pivot pode ser qualquer posição
-    console.log('o pivot é: ')
-    let div = ini - 1;
-
-    for(let i=ini; i<fim; i++){
-        if(vetor[pivot]>vetor[i]){
-            div++ // se o número iterado é maior que o pivot, o div anda
-            if(div !== i){
-                [vetor[i], vetor[div]] = [vetor[div], vetor[i]]
-            };
-        };
-    };
-    div++;
-    if(vetor[div] > vetor[pivot] && div !== pivot){
-        [vetor[pivot], vetor[div]] = [vetor[div], vetor[pivot]]
-    };
-
-    // Após isso, do div pra frente serão os maiores que o pivot, do div pra trás serão os menores.
-
-    quickSort(vetor, ini, div-1) // Menores que o pivot (esquerda)
-    quickSort(vetor, div+1, fim) // Maiores que o pivot (direita)
-};
 const nums = [2,5,7,1,6,3,4]
 // const nums = [3,4,2,1]
 
