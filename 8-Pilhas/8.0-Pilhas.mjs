b4718e74fd2b7e2003b9aa903ce0d99e7bc68646
Joao Victor <joaovictorxl120@gmail.com>
2024-05-09 19:48:55 -0300

    pilhas

---
+++ 8-Pilhas/8.0-Pilhas.mjs
@@ -0,0 +1,10 @@
+/* 
+    Pilhas são estruturas que armazenam dados usando a estratégia LIFO (Last In, First Out).
+    O último a entrar será o primeiro a sair, como uma pilha de pratos, por exemplo, pois você precisa retirar o
+    primeiro para ter acesso aos últimos. 
+    
+    Pilhas são usadas em tarefas computacionais que requem a INVERSÃO DA ORDEM DE ENTRADA DOS DADOS
+
+    BASICAMENTE: OPERAÇÕES DDE INSERÇÃO E REMOÇÃO SOMENTE NO FINAL!
+*/
+
